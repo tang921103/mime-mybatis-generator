66daf12e1ed7db8db86eed9d41c6e7ec5168bbd9
tangqiu <tangqiuemail@sina.cn>
2018-11-05 20:18:35 +0800

    第一次

---
+++ src/main/java/com/exam/mimemybatisgenerator/MimeMybatisGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.exam.mimemybatisgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimeMybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimeMybatisGeneratorApplication.class, args);
+    }
+}
